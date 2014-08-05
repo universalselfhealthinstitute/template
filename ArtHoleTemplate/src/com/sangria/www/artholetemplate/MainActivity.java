b8ea4d7790d1cdc19cf0096874ec121834326036
artholeapps <mattsandler@computers-air.lan>
2014-08-04 22:10:03 -0400

    Fixed title from menu not tabs

--- ArtHoleTemplate/src/com/sangria/www/artholetemplate/MainActivity.java
+++ ArtHoleTemplate/src/com/sangria/www/artholetemplate/MainActivity.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2012 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 package com.sangria.www.artholetemplate;
 
@@ -446,6 +431,8 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
     			position = 0;
 
     			 mViewPager.setCurrentItem(position);
+    			 
+    			 setTitle(getString(R.string.section_1_title));
 
     			return true;
 
@@ -454,14 +441,19 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
     			position = 1;
 
     			mViewPager.setCurrentItem(position);
+    			
+    			setTitle(getString(R.string.section_2_title));
 
     			return true;
+    			
 
     		case R.id.menu_image:
 
     			position = 2;
 
     			mViewPager.setCurrentItem(position);
+    			
+    			setTitle(getString(R.string.section_3_title));
 
     			return true;
 
@@ -470,6 +462,8 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
     			position = 3;
 
     			mViewPager.setCurrentItem(position);
+    			
+    			setTitle(getString(R.string.section_4_title));
 
     			return true;
 
@@ -478,10 +472,13 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
     			position = 4;
 
     			mViewPager.setCurrentItem(position);
+    			
+    			setTitle(getString(R.string.section_5_title));
 
     			return true;
 
     		default:
+    			
     			return super.onOptionsItemSelected(item);
 
     		}
